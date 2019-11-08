a8495289ba1e307fa55fed3b2c774270f096f085
shaoyapeng <1078547668@qq.com>
2019-11-08 21:28:49 +0800

    auth

---
+++ uxnux-server-cloud/uxnux-auth/src/main/java/com/uxnux/auth/UxnuxAuthApplication.java
@@ -0,0 +1,13 @@
+package com.uxnux.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UxnuxAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UxnuxAuthApplication.class, args);
+    }
+
+}
---
+++ uxnux-server-cloud/uxnux-auth/src/test/java/com/uxnux/auth/UxnuxAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uxnux.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UxnuxAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
