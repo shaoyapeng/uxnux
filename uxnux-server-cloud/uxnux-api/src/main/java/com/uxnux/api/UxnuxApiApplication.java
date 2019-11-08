f139c0848ca7658297aa2adfaead60b43730d6d3
shaoyapeng <1078547668@qq.com>
2019-11-08 12:03:02 +0800

    api

--- uxnux-server-cloud/uxnux-api/src/main/java/com/uxnux/api/UxnuxApiApplication.java
+++ uxnux-server-cloud/uxnux-api/src/main/java/com/uxnux/api/UxnuxApiApplication.java
@@ -2,8 +2,10 @@ package com.uxnux.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class UxnuxApiApplication {
 
     public static void main(String[] args) {
--- uxnux-server-cloud/uxnux-api/src/main/java/com/uxnux/api/controller/ApiTestController.java
+++ uxnux-server-cloud/uxnux-api/src/main/java/com/uxnux/api/controller/ApiTestController.java
@@ -1,12 +1,18 @@
 package com.uxnux.api.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * @Author: 10785
  * @Date: 2019/11/8 9:57
  * @Version: 1.0
  */
-
+@RestController
 public class ApiTestController {
 
-
+    @RequestMapping("/test")
+    public String getTest () {
+        return "test";
+    }
 }
