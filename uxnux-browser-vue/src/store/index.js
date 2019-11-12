e74e925e22a194ab4974d5cd091ecfe8f0461fe4
shaoyapeng <1078547668@qq.com>
2019-11-12 11:04:47 +0800

    2111

--- uxnux-browser-vue/src/store/index.js
+++ uxnux-browser-vue/src/store/index.js
@@ -1,11 +1,34 @@
-import Vue from "vue";
-import Vuex from "vuex";
+import Vue from "vue"
+import Vuex from "vuex"
+
+const state = {
+  theme: ""
+}
+
+const getters = {
+  getTheme(state) {
+    return state.theme
+  }
+}
+
+const mutations = {
+  setTheme(state, theme) {
+    state.theme = theme
+  }
+}
+
+const actions = {
+  asyncSetTheme: (context, theme) => {
+    context.commit('theme', theme)
+  },
+}
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
+  state: state,
+  getters: getters,
+  mutations: mutations,
+  actions: actions,
   modules: {}
-});
+})
